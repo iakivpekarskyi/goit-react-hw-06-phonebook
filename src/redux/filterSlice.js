5854bc27c45f9902a5b78bbfa1503a845ae72eba
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-08-23 15:02:42 +0200

    додав коментар в filterSlice

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -7,8 +7,25 @@ const filterSlice = createSlice({
     setFilter: (state, action) => {
       state.filter = action.payload;
     },
+
+    //якщо я роблю так, то він каже, що не можна міняти стейт напряму(хоча я думав, що тут можна використовувати inner)
+
+    ////////////////////////////////////////
+
+    // якщо робити имутабельно, то теж не працює :
+
+    //     setFilter(state, { payload }) {
+    //   return {
+    //     ...state,
+    //     filter: payload,
+    //   };
+    // }
+
+    clearFilter() {
+      return '';
+    },
   },
 });
 
-export const { setFilter } = filterSlice.actions;
+export const { setFilter, clearFilter } = filterSlice.actions;
 export const filterReducer = filterSlice.reducer;
