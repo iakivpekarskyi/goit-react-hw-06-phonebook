0d3af9c311a5d3d8de06467802c6cbdf9c8054b4
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-08-23 21:27:15 +0200

    fixed but not working

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -2,24 +2,29 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const filterSlice = createSlice({
   name: 'filter',
-  initialState: '',
+  initialState: {
+    value: "",
+  },
   reducers: {
     setFilter: (state, action) => {
-      state.filter = action.payload;
+ 
+        // всі ці три варіанти не працюють
+      
+      
+// state.filter = action.payload;
+
+// return {
+//         ...state,
+//         value: action.payload,
+//       };
+
+
+
+      return {
+        ...state,
+      state.value = action.payload,
+      };
     },
-
-    //якщо я роблю так, то він каже, що не можна міняти стейт напряму(хоча я думав, що тут можна використовувати inner)
-
-    ////////////////////////////////////////
-
-    // якщо робити имутабельно, то теж не працює :
-
-    //     setFilter(state, { payload }) {
-    //   return {
-    //     ...state,
-    //     filter: payload,
-    //   };
-    // }
 
     clearFilter() {
       return '';
