01c8238a2a40ba96efea747c50a312fc0e24b135
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-08-25 11:51:09 +0200

    done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,18 @@
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import { ContactListWrap } from './ContactList.styled';
 import { useSelector } from 'react-redux';
-import { getContacts } from 'redux/selectors';
+import { getContacts, getFilter } from 'redux/selectors';
 
 export const ContactList = () => {
   const contacts = useSelector(getContacts);
+  const Filter = useSelector(getFilter);
+  const filtered = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(Filter.toLowerCase())
+  );
 
   return (
     <ContactListWrap>
-      {contacts.map(({ id, name, number }) => {
+      {filtered.map(({ id, name, number }) => {
         return <ContactListItem key={id} name={name} number={number} id={id} />;
       })}
     </ContactListWrap>
--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -3,31 +3,15 @@ import { createSlice } from '@reduxjs/toolkit';
 const filterSlice = createSlice({
   name: 'filter',
   initialState: {
-    value: "",
+    value: '',
   },
   reducers: {
     setFilter: (state, action) => {
- 
-        // всі ці три варіанти не працюють
-      
-      
-// state.filter = action.payload;
-
-// return {
-//         ...state,
-//         value: action.payload,
-//       };
-
-
-
-      return {
-        ...state,
-      state.value = action.payload,
-      };
+      state.value = action.payload;
     },
 
-    clearFilter() {
-      return '';
+    clearFilter: state => {
+      state.value = '';
     },
   },
 });
